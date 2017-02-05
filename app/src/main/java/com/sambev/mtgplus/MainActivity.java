036e6107d2941586309740885425f60505c87f6e
Sam Beveridge <sbeveridge@saltstack.com>
2017-02-04 19:50:17 -0700

    Add remove one life handler.

--- app/src/main/java/com/sambev/mtgplus/MainActivity.java
+++ app/src/main/java/com/sambev/mtgplus/MainActivity.java
@@ -13,10 +13,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Add one to the life_amount view
+     */
     public void addLife(View view) {
         TextView lifeView = (TextView) findViewById(R.id.life_amount);
         int lifeAmount = Integer.parseInt(lifeView.getText().toString());
         String newLifeAmount = Integer.toString(lifeAmount + 1);
         lifeView.setText(newLifeAmount);
     }
+
+    /**
+     * Remove one life from life_amount view
+     */
+    public void removeLife(View view) {
+        TextView lifeView = (TextView) findViewById(R.id.life_amount);
+        int lifeAmount = Integer.parseInt(lifeView.getText().toString());
+        String newLifeAmount = Integer.toString(lifeAmount - 1);
+        lifeView.setText(newLifeAmount);
+    }
+
 }
