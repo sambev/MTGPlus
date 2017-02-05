a4a0d5f9c7fc0b9aac98009685ef0ebd27d4ee0e
Sam Beveridge <sbeveridge@saltstack.com>
2017-02-04 19:47:03 -0700

    Add the addLife logic to add life.

--- app/src/main/java/com/sambev/mtgplus/MainActivity.java
+++ app/src/main/java/com/sambev/mtgplus/MainActivity.java
@@ -2,6 +2,8 @@ package com.sambev.mtgplus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addLife(View view) {
+        TextView lifeView = (TextView) findViewById(R.id.life_amount);
+        int lifeAmount = Integer.parseInt(lifeView.getText().toString());
+        String newLifeAmount = Integer.toString(lifeAmount + 1);
+        lifeView.setText(newLifeAmount);
     }
 }
